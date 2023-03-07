2c2e6f95577c0b3a5ca696ae6506167401201dea
Dino1984WB <williamkbukowskidev@gmail.com>
2023-03-07 18:17:10 -0500

    more commits

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #William Bukowski, author: Basics in ML
+
+#this implemention will utilize scikit-learn open source library
+
+from sklearn import datasets
+from sklearn import svm
+
+clf = svm.SVC(gamma=0.001, c=100.)
